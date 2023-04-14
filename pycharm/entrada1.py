140800c218b7e5ea512a10d4dfeb97fc22023fb8
Bruno <brunocosta.1991@hotmail.com>
2023-04-13 21:20:48 -0300

    Exemplo 1

---
+++ pycharm/entrada1.py
@@ -0,0 +1,14 @@
+print('Digite o valor 1: ')
+valor1 = float(input())
+print('Digite o valor 2:')
+valor2 = float(input())
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+
+print('Soma: ', soma)
+print('Subtração: ', subtracao)
+print('Multiplicação: ', multiplicacao)
+print('Divisão: ', divisao)
