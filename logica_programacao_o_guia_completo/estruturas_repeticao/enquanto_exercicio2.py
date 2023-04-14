4c97d281c949be9d08aeebce79d1f6b8ed29a98b
Bruno <brunocosta.1991@hotmail.com>
2023-04-14 19:52:59 -0300

    Acumulador 1 a 100

---
+++ logica_programacao_o_guia_completo/estruturas_repeticao/enquanto_exercicio2.py
@@ -0,0 +1,13 @@
+"""
+(Acumulador) apresentar o total de soma obtida dos cem primeiros números inteiros
+(1+2+3+4+5+6+7+....+97+98+99+100)
+"""
+
+contador = 1
+soma = 0
+while contador <= 100:
+    soma = soma + contador
+    contador += 1
+
+print('Somatório dos 100 primeiros números: ', soma)
+
