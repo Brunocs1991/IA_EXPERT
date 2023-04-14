b4fa3838a78a6f01ef71374f0292439670e49eb7
Bruno <brunocosta.1991@hotmail.com>
2023-04-13 22:31:13 -0300

    case vogal ou consoante

---
+++ logica_programacao_o_guia_completo/estruturas_condicionais/case_exercicio1.py
@@ -0,0 +1,16 @@
+print('Digite uma letra: ')
+letra = input()
+
+match letra:
+    case 'a':
+        print('Vogal')
+    case 'e':
+        print('Vogal')
+    case 'i':
+        print('Vogal')
+    case 'o':
+        print('Vogal')
+    case 'u':
+        print('Vogal')
+    case _:
+        print('Consoante')
