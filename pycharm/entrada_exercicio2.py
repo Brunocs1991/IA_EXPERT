820c9d47c05c8a34fe3672d1ed1206af650d5bfd
Bruno <brunocosta.1991@hotmail.com>
2023-04-13 21:27:57 -0300

    exercicio 1 Calculo volume

---
+++ pycharm/entrada_exercicio2.py
@@ -0,0 +1,9 @@
+print('Digite o valor do comprimento: ')
+comprimento = float(input())
+print('Digite o valor da largura: ')
+largura = float(input())
+print('Digite o valor da altura: ')
+altura = float(input())
+
+volume = comprimento * altura * largura;
+print('Volume: ', volume)
